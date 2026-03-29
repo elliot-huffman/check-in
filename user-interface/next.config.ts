63e1fec653754d61771345c06113ecb561e5db9f
Elliot Huffman <elliot@huffman.me>
2026-03-29 19:53:39 -0400

    Configure User Interface Compile
    Add the compile options required for low risk coding with high quality code checks and syntax enforcement at the compiler level.
    Configure Next.JS with support for React compiler, turbopack, and static host integration.

---
+++ user-interface/next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+/** Configurations used for the Next.js compiler. */
+const nextConfig: NextConfig = {
+    // Ensure that the output folder is cleaned before each build to prevent stale files from persisting.
+    'cleanDistDir': true,
+    // Compile to static HTML files for external integration
+    'output': 'export',
+    // Auto optimize code so that the UI looks more like native JS instead of react hook code.
+    'reactCompiler': true,
+    // Enable an additional layer of logical checks and warnings in development mode to help identify potential issues early on.
+    'reactStrictMode': true,
+    // Ensure that all generated URLs end with a trailing slash, which can help with consistent URL structure for static hosting.
+    'trailingSlash': true
+}
+
+// Export the configuration object so that Next.js can use it during the build and development processes.
+export default nextConfig
