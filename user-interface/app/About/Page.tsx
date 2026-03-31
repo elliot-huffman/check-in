434f32b263a2f7cf7bd19ba12ef5e330c8d09b5f
Elliot Huffman <elliot@huffman.me>
2026-03-30 20:29:03 -0400

    Place Holders
    Add home and about page place holders.

---
+++ user-interface/app/About/Page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+/**
+ * Renders the about page that contains legal notices.
+ * @returns Rendered page.
+ */
+export default function Page(): React.ReactNode {
+    return (
+        'Hello world!'
+    );
+}
---
+++ user-interface/app/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { Layout, LayoutItem } from './_components/LayoutSystem';
+import { Text } from '@fluentui/react-components';
+
+/**
+ * Renders the main/home page of the application.
+ * @returns Rendered main page for the application.
+ */
+export default function Page(): React.ReactNode {
+    return (
+        <Layout direction="row">
+            <LayoutItem direction="column">
+                <Text>Hello, World - item!</Text>
+                <Text>Hello, World - item!</Text>
+            </LayoutItem>
+            <Text>Hello, World!</Text>
+            <Text>Hello, World!</Text>
+            <Text>Hello, World!</Text>
+            <Text>Hello, World!</Text>
+            <Text>Hello, World!</Text>
+        </Layout>
+    );
+}
