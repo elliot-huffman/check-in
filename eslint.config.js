685b67f226bf321b7546217a3d893e942fd04e31
Elliot Huffman <elliot@huffman.me>
2026-04-19 22:06:08 -0400

    Ignore Compiled Output
    Fix the lint config to ensure the UI's compiled output and cache are not included in the linting results.
    Additionally the public folder (files included verbatim in the final build) are ignored as these will mostly be images and HTML content that should not be linted int he first place.

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,14 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
 import { eslintConfig as baseConfig } from '@shi-corp/development-utilities/optimized/lint/base.js';
 import { eslintConfig as nextConfig } from '@shi-corp/development-utilities/optimized/lint/next.js';
-import { defineConfig } from 'eslint/config';
 
 // Linting configuration used for the runtime and UI as defined by SHI.
 export default defineConfig([
+    globalIgnores([
+        'user-interface/.next/**',
+        'user-interface/out/**',
+        'user-interface/public/**'
+    ]),
     ...baseConfig.map(config => ({ ...config, 'files': ['runtime/**/*.{ts,js}'] })),
     ...nextConfig.map(config => ({ ...config, 'files': ['user-interface/**/*.{ts,js,tsx}'] })),
 ]);
