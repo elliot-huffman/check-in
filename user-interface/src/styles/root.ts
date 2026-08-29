2a1b0c39d7accf90d7f035f63beb12386f5e3d83
Elliot Huffman <elliot@huffman.me>
2026-08-28 23:22:32 -0400

    Formatting

--- user-interface/src/styles/root.ts
+++ user-interface/src/styles/root.ts
@@ -2,15 +2,11 @@ import { makeStyles, tokens } from '@fluentui/react-components';
 
 /** List of CSS styles for the global layout component. */
 export const useStyleList = makeStyles({
-    'body': {
-        'margin': 0
-    },
-    'themeProvider': {
-        'minHeight': '100vh'
-    },
+    'body': { 'margin': 0 },
     'html': {
         'backgroundColor': tokens.colorNeutralBackground1,
         'overflow': 'hidden',
         'scrollbarGutter': 'unset'
-    }
+    },
+    'themeProvider': { 'minHeight': '100vh' }
 });
